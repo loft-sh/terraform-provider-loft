c054b177ceae2e59b8ed1ea472e325e78fabd5ff
Russell Centanni <russell.centanni@gmail.com>
2022-05-13 12:52:39 -0400

    test: update datasource tests to first create resources and then load them

--- internal/provider/data_source_virtual_clusters_test.go
+++ internal/provider/data_source_virtual_clusters_test.go
@@ -1,14 +1,14 @@
 package provider
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"k8s.io/apiserver/pkg/storage/names"
 	"regexp"
 	"strings"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"k8s.io/apiserver/pkg/storage/names"
 )
 
 func TestAccDataSourceVirtualClusters_all(t *testing.T) {
@@ -17,16 +17,18 @@ func TestAccDataSourceVirtualClusters_all(t *testing.T) {
 	team := "loft-admins"
 	clusterName := "loft-cluster"
 	namespace := "testn1s"
-	virtualCluster1Name := names.SimpleNameGenerator.GenerateName("my-virtual-cluster-1-")
-	virtualCluster2Name := names.SimpleNameGenerator.GenerateName("my-virtual-cluster-2-")
-	virtualCluster3Name := names.SimpleNameGenerator.GenerateName("my-virtual-cluster-3-")
 
+	virtualCluster1Name := names.SimpleNameGenerator.GenerateName("my-virtual-cluster-1-")
 	annotation := names.SimpleNameGenerator.GenerateName("annotation-")
+
+	virtualCluster2Name := names.SimpleNameGenerator.GenerateName("my-virtual-cluster-2-")
 	label := names.SimpleNameGenerator.GenerateName("label-")
+
+	virtualCluster3Name := names.SimpleNameGenerator.GenerateName("my-virtual-cluster-3-")
 	values := `storage:
   size: 5Gi
 `
-	objectsName := names.SimpleNameGenerator.GenerateName("objects-")
+	virtualCluster4Name := names.SimpleNameGenerator.GenerateName("objects-")
 	objects := `apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -71,72 +73,101 @@ data:
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceVirtualClusterCreateWithAnnotations(configPath, clusterName, namespace, virtualCluster1Name, annotation),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("loft_virtual_cluster.test_annotations", "cluster", clusterName),
-					resource.TestCheckResourceAttr("loft_virtual_cluster.test_annotations", "name", virtualCluster1Name),
-				),
-			},
-			{
-				Config: testAccDataSourceVirtualClusterCreateWithLabels(configPath, clusterName, namespace, virtualCluster2Name, label),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("loft_virtual_cluster.test_labels", "cluster", clusterName),
-					resource.TestCheckResourceAttr("loft_virtual_cluster.test_labels", "name", virtualCluster2Name),
-				),
-			},
-			{
-				Config: testAccDataSourceVirtualClusterCreateWithVirtualClusterValues(configPath, clusterName, namespace, virtualCluster3Name, values),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("loft_virtual_cluster.test_values", "cluster", clusterName),
-					resource.TestCheckResourceAttr("loft_virtual_cluster.test_values", "name", virtualCluster3Name),
-				),
-			},
-			{
-				Config: testAccDataSourceVirtualClusterCreateWithVirtualClusterObjects(configPath, clusterName, namespace, objectsName, objects),
+				Config: testAccDataSourceVirtualClustersCreate(configPath, clusterName, namespace, virtualCluster1Name, annotation, virtualCluster2Name, label, virtualCluster3Name, values, virtualCluster4Name, objects),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("loft_virtual_cluster.test_objects", "cluster", clusterName),
-					resource.TestCheckResourceAttr("loft_virtual_cluster.test_objects", "name", objectsName),
+					resource.TestCheckResourceAttr("loft_virtual_cluster.test_objects", "name", virtualCluster4Name),
 				),
 			},
 			{
-				Config: testAccDataSourceVirtualClustersAll(configPath, clusterName, namespace),
+				Config: testAccDataSourceVirtualClustersCreate(configPath, clusterName, namespace, virtualCluster1Name, annotation, virtualCluster2Name, label, virtualCluster3Name, values, virtualCluster4Name, objects) +
+					testAccDataSourceVirtualClustersAll(clusterName, namespace),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr("data.loft_virtual_clusters.all", "virtual_clusters.#", rxPosNum),
-					//checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster1Name, "name", virtualCluster1Name),
-					//checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster1Name, "cluster", clusterName),
-					//checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster2Name, "name", virtualCluster2Name),
-					//checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster2Name, "cluster", clusterName),
-					//checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster3Name, "name", virtualCluster3Name),
-					//checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster3Name, "cluster", clusterName),
-					checkVirtualClusterByName("data.loft_virtual_clusters.all", objectsName, "name", objectsName),
-					checkVirtualClusterByName("data.loft_virtual_clusters.all", objectsName, "cluster", clusterName),
-					checkVirtualClusterByName("data.loft_virtual_clusters.all", objectsName, "objects", objects),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster1Name, "name", virtualCluster1Name),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster1Name, "cluster", clusterName),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster2Name, "name", virtualCluster2Name),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster2Name, "cluster", clusterName),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster3Name, "name", virtualCluster3Name),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster3Name, "cluster", clusterName),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster4Name, "name", virtualCluster4Name),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster4Name, "cluster", clusterName),
+					checkVirtualClusterByName("data.loft_virtual_clusters.all", virtualCluster4Name, "objects", objects),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceVirtualClustersAll(configPath string, clusterName string, namespace string) string {
+func testAccDataSourceVirtualClustersCreate(configPath, clusterName, namespace, virtualCluster1Name, annotation, virtualCluster2Name, labels, virtualCluster3Name, values, virtualCluster4Name, objects string) string {
 	return fmt.Sprintf(`
 terraform {
-required_providers {
-   loft = {
-      source = "registry.terraform.io/loft-sh/loft"
-   }
-}
+	required_providers {
+		loft = {
+			source = "registry.terraform.io/loft-sh/loft"
+		}
+	}
 }
 
 provider "loft" {
-config_path = "%s"
+	config_path = "%s"
 }
 
-data "loft_virtual_clusters" "all" {
-cluster = "%s"
-namespace = "%s"
+resource "loft_virtual_cluster" "test_annotations" {
+	name = "%[4]s"
+	cluster = "%[2]s"
+	namespace = "%[3]s"
+	annotations = {
+		"some.domain/test" = "%[5]s"
+	}
+}
+
+resource "loft_virtual_cluster" "test_labels" {
+	name = "%[6]s"
+	cluster = "%[2]s"
+    namespace = "%[3]s"
+	labels = {
+		"some.domain/test" = "%[7]s"
+	}
+}
+
+resource "loft_virtual_cluster" "test_values" {
+	name = "%[8]s"
+	cluster = "%[2]s"
+    namespace = "%[3]s"
+	values = <<YAML
+%[9]sYAML
+}
+
+resource "loft_virtual_cluster" "test_objects" {
+	name = "%[10]s"
+	cluster = "%[2]s"
+    namespace = "%[3]s"
+	objects = <<YAML
+%[11]sYAML
 }
 `,
 		configPath,
+		clusterName,
+		namespace,
+		virtualCluster1Name,
+		annotation,
+		virtualCluster2Name,
+		labels,
+		virtualCluster3Name,
+		values,
+		virtualCluster4Name,
+		objects,
+	)
+}
+
+func testAccDataSourceVirtualClustersAll(clusterName string, namespace string) string {
+	return fmt.Sprintf(`
+data "loft_virtual_clusters" "all" {
+	cluster = "%s"
+	namespace = "%s"
+}
+`,
 		clusterName,
 		namespace,
 	)
@@ -147,9 +178,7 @@ func checkVirtualClusterByName(moduleName, virtualClusterName, key, value string
 		virtualClusterPath := ""
 		virtualClusterNameMatch := regexp.MustCompile(`virtual_clusters\.\d+\.name`)
 		primaryModule := s.RootModule().Resources[moduleName].Primary
-		//fmt.Println("primaryModule : ", primaryModule.Attributes)
-		marshal, _ := json.Marshal(primaryModule)
-		fmt.Println(string(marshal))
+
 		for k, v := range primaryModule.Attributes {
 			if virtualClusterNameMatch.MatchString(k) && v == virtualClusterName {
 				tokens := strings.Split(k, ".")
